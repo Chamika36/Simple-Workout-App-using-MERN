359fb1fdc41dd5d8e99a6fea52c2027cfca9ab39
Chamika Madhushan <chamikamadhushan35@gmail.com>
2023-11-16 21:33:40 +0530

    Added workoutform

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import WorkoutDetails from '../components/WorkoutDetails'
-
+import WorkoutForm from '../components/Workoutform';
 
 const Home = () => {
     const[workouts, setWorkouts]  = useState(null)
@@ -25,6 +25,7 @@ const Home = () => {
                     <WorkoutDetails key={workout._id} workout={workout}/>
                 ))}
             </div>
+            <WorkoutForm />
         </div>
     );
 }
